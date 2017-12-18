0a0339e0efc558d5e0b481165b4e53b5a11c789e
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-12-18 15:30:26 -0500

    File writing

--- OrcMathGUI/src/guiPlayer/CatalogueMaker.java
+++ OrcMathGUI/src/guiPlayer/CatalogueMaker.java
@@ -1,5 +1,7 @@
 package guiPlayer;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -36,22 +38,39 @@ public class CatalogueMaker
 		list.add(new sortingAlgorithm(name, complexity, type, price));
 	}
 	
+	private void testSaveContent(String fileName, String content) {
+		try{    
+			FileWriter fw=new FileWriter(fileName);    
+			fw.write(content);    
+			fw.close();    
+			System.out.println("Success! File \""+fileName+"\" saved!");
+		}catch(IOException e){
+			System.out.println("An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
+		}
+
+	}
+	
 	public static void main(String[] args)
 	{
 		CatalogueMaker catalogue = new CatalogueMaker();
 		System.out.println(catalogue.getCsvContent());
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter name of item to add:");
-		String name = in.nextLine();
-		System.out.println("Enter complexity of item to add:");
-		String complexity = in.nextLine();
-		System.out.println("Enter type of item to add:");
-		String type = in.nextLine();
-		System.out.println("Enter price of item to add:");
-		int price = Integer.parseInt(in.nextLine());
-		catalogue.addNewItem(name, complexity, type, price);
-		System.out.println("Added new item.");
-		System.out.println(catalogue.getCsvContent());
+		
+		catalogue.testSaveContent("sorting.csv", catalogue.getCsvContent());
+		
+//		Scanner in = new Scanner(System.in);
+//		System.out.println("Enter name of item to add:");
+//		String name = in.nextLine();
+//		System.out.println("Enter complexity of item to add:");
+//		String complexity = in.nextLine();
+//		System.out.println("Enter type of item to add:");
+//		String type = in.nextLine();
+//		System.out.println("Enter price of item to add:");
+//		int price = Integer.parseInt(in.nextLine());
+//		catalogue.addNewItem(name, complexity, type, price);
+//		System.out.println("Added new item.");
+//		System.out.println(catalogue.getCsvContent());
+		
+		
 	}
 
 }
