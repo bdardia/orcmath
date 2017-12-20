4b4623d8d400e63c73dc5c60ec5a35e26c246828
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-12-20 14:26:18 -0500

    Added loading

--- OrcMathGUI/src/guiPlayer/CatalogueMaker.java
+++ OrcMathGUI/src/guiPlayer/CatalogueMaker.java
@@ -1,8 +1,12 @@
 package guiPlayer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CatalogueMaker 
@@ -38,7 +42,7 @@ public class CatalogueMaker
 		list.add(new sortingAlgorithm(name, complexity, type, price));
 	}
 	
-	private void testSaveContent(String fileName, String content) {
+	public void testSaveContent(String fileName, String content) {
 		try{    
 			FileWriter fw=new FileWriter(fileName);    
 			fw.write(content);    
@@ -48,6 +52,45 @@ public class CatalogueMaker
 			System.out.println("An IOException was thrown. \nCheck to see that the directory where you tried to save the file actually exists.");
 		}
 
+	}
+	
+	public List<String> testFileLoading() {
+		Scanner in = new Scanner(System.in);
+		String fileName = "";
+		List<String> content = new ArrayList<String>();
+		//use this boolean to control the while loop. The user should have multiple chances to enter a correct filename
+		boolean opened = false;
+		while(!opened){
+			try {
+				System.out.println("Enter a file to open");
+				fileName = in.nextLine();
+				FileReader fileReader = new FileReader(new File(fileName));
+				String line = "";
+				//a BufferedReader enables us to read teh file one line at a time
+				BufferedReader br = new BufferedReader(fileReader);
+				while ((line = br.readLine()) != null) {
+
+					content.add(line);
+
+					/*
+					 * useful later:
+					 * split only a comma that has an even number of quotes ahead of it
+ String[] row = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+					 */
+
+
+				}
+				br.close();
+				opened = true;
+			}catch (IOException e) {
+				System.out.println("The file name you specified does not exist.");
+			}
+		}
+
+
+		//close the Scanner
+		in.close();
+		return content;
 	}
 	
 	public static void main(String[] args)
