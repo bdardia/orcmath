4c51dfc14db2da059343fac718cbf0df24cb3998
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-12-20 14:34:27 -0500

    asdf

---
+++ OrcMathGUI/src/guiPlayer/CatalogueScreen.java
@@ -0,0 +1,29 @@
+package guiPlayer;
+
+import java.util.List;
+
+import guiTeacher.components.AnimatedComponent;
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class CatalogueScreen extends FullFunctionScreen{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 258186143576427947L;
+
+		public CatalogueScreen(int width, int height) 
+		{
+			super(width, height);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void initAllObjects(List<Visible> viewObjects) 
+		{
+			// TODO Auto-generated method stub
+			
+		}
+			//Set styles
+}
