8d92843ef5159c173f6dc213cfb4e981c263a777
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-02 15:30:54 -0500

    sdfg

--- OrcMathGUI/src/bdardia/CustomButton.java
+++ OrcMathGUI/src/bdardia/CustomButton.java
@@ -1,21 +1,17 @@
 package bdardia;
 
 import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
 
-import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 
 public class CustomButton extends Button implements CustomLabel
 {
 
 	public CustomButton(int x, int y) {
-		super(x, y, 40, 200, "Save", new Action() {			
-			@Override
-			public void act() {
-				// TODO Auto-generated method stub
-				
-			}
-		});
+		super(x,y, 300,100,"",null);
+		update();
 		// TODO Auto-generated constructor stub
 	}
 
@@ -35,6 +31,24 @@ public class CustomButton extends Button implements CustomLabel
 	public void setIconColor(Color color) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public void drawButton(Graphics2D g, boolean hover)
+	{
+		g.setColor(Color.BLACK);
+		FontMetrics fm = g.getFontMetrics();
+		
+		g.setColor(getForeground());
+		String t = "Save";
+		//just in case text is too wide, cut off
+		int cutoff = t.length();
+		while(cutoff > 0 && fm.stringWidth(t) > getWidth()){
+			cutoff --;
+			t = t.substring(0,cutoff); 
+		}
+		g.drawString(t, (getWidth()-fm.stringWidth(t))/2, 
+				(getHeight()+fm.getHeight()-fm.getDescent())/2);
 	}
 	
 	
