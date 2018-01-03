7666f78ed25e03baf2da6e0513e004156bdf475a
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-03 14:52:51 -0500

    adf

--- OrcMathGUI/src/bdardia/SimonGameBenjamin.java
+++ OrcMathGUI/src/bdardia/SimonGameBenjamin.java
@@ -2,17 +2,20 @@ package bdardia;
 
 import guiTeacher.GUIApplication;
 
-public class SimonGameBenjamin extends GUIApplication {
-
-	public SimonGameBenjamin(int width, int height) {
+public class SimonGameBenjamin extends GUIApplication 
+{
+	private SimonScreenBenjamin simonScreen;
+	
+	public SimonGameBenjamin(int width, int height)
+	{
 		super(width, height);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void initScreen() {
-		// TODO Auto-generated method stub
-
+	public void initScreen()
+	{
+		
 	}
 
 }
