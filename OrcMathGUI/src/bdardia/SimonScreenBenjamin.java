217c04faf83fc66e59f5c2cce4bc9683374abc2f
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-03 14:51:07 -0500

    Started simon
    \

--- OrcMathGUI/src/bdardia/SimonScreenBenjamin.java
+++ OrcMathGUI/src/bdardia/SimonScreenBenjamin.java
@@ -1,25 +1,34 @@
 package bdardia;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.ClickableScreen;
 
-public class SimonScreenBenjamin extends ClickableScreen implements Runnable {
-
-	public SimonScreenBenjamin(int width, int height) {
+public class SimonScreenBenjamin extends ClickableScreen implements Runnable 
+{
+	private ProgressInterfaceBenjamin progress;
+	private ArrayList<MoveInterfaceBenjamin> buttons;
+	private int roundNumber;
+	private boolean userInput;
+	
+	public SimonScreenBenjamin(int width, int height) 
+	{
 		super(width, height);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void run() {
+	public void run() 
+	{
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void initAllObjects(List<Visible> viewObjects) {
+	public void initAllObjects(List<Visible> viewObjects)
+	{
 		// TODO Auto-generated method stub
 
 	}
